0c024672eb43d06a534dd939c85682ab2fb30d3a
elizarowoy <waikastreet@gmail.com>
2017-04-04 16:10:32 +0300

    Hello world!

--- eApp/eApp/Program.cs
+++ eApp/eApp/Program.cs
@@ -10,9 +10,26 @@ namespace eApp
     {
         static void Main(string[] args)
         {
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
 
-            Console.WriteLine("Hello world");
+            Draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
+
+
+
             Console.ReadLine();
+        }
+        static void Draw(int y, int x, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
